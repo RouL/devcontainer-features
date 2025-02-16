b5b4111f385bc43cad3b232e599f93963619a416
Markus Zhang <markus@zett.dev>
2025-02-16 00:49:47 +0000

    [CHANGE] Reduced test scenarios for kamal

--- test/kamal/mcr-ruby3.3-kamal2.4.0.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-source dev-container-features-test-lib
-source functions.sh
-
-check_kamal_version "2.4.0"
-
-reportResults
--- test/kamal/rails-ruby3.3.7-kamal2.5.0.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-source dev-container-features-test-lib
-source functions.sh
-
-check_kamal_version "2.5.0"
-
-reportResults

43edcfbcc6a36037ec495cde9f167b8ba40076de
Markus Zhang <zhang@paritaet-nrw.org>
2025-05-15 08:43:22 +0000

    [FIX] mise-java test

--- test/mise-java/test.sh
+++ test/mise-java/test.sh
@@ -3,6 +3,7 @@ set -e
 
 source dev-container-features-test-lib
 
-check "check default java" bash -c "java -version 2>&1 | grep -E '^openjdk version \"[1-9][0-9]*\"'"
+java -version 2>&1
+check "check default java" bash -c "java -version 2>&1 | grep -E '^openjdk version \"[1-9][0-9]*'"
 
 reportResults
